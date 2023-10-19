15f89c9031a3c9961090f3db382ab5690848304a
MvMukesh <mukeshmanral777@gmail.com>
2023-10-19 10:34:16 +0530

    created requirements.txt and setup.py files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'ObjectDetection-Framework-YOLOv5_Pipeline',
+    version= '0.0.1',
+    author= 'Mukesh Manral',
+    author_email= 'mukeshmanral777@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
